7997d7aee2902389354897c5ec7bc18de7275d51
niedzwiedzw <niedzwiedzwo@gmail.com>
2024-09-12 22:23:52 +0200

    quick rebuild

--- home-manager/sway.nix
+++ home-manager/sway.nix
@@ -79,7 +79,7 @@
       output * bg /home/niedzwiedz/nixos/my-wallpaper-malysz-tajner-chester-linkin-park.png fill
 
       # keybinds
-      bindsym Mod4+p exec grim -g "$(slurp -d)" - | swappy -f - -o - | pngquant - | wl-copy -t image/png
+      bindsym Mod4+Shift+x exec grim -g "$(slurp -d)" - | swappy -f - -o - | pngquant - | wl-copy -t image/png
     '';
 
     config = {
