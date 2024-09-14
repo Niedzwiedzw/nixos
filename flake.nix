daaffe783e29c1c0d6c839193d895b06238cc8a4
niedzwiedzw <niedzwiedzwo@gmail.com>
2024-09-14 13:52:10 +0200

    quick rebuild

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/master";
     };
     musnix = {url = "github:musnix/musnix";};
     stylix.url = "github:danth/stylix";
