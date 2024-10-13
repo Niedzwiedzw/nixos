670bfa6cd5bfb59ed40c5bb25b4dbf2f92df9c1e
niedzwiedzw <niedzwiedzwo@gmail.com>
2024-10-13 13:53:08 +0200

    quick rebuild

--- configuration.nix
+++ configuration.nix
@@ -227,6 +227,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    # backups
+    borgmatic
     # raid
     mdadm
     # devtools
