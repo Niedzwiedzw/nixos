7da226cbf6f82451a2052e0cd06c1511a919d47d
niedzwiedzw <niedzwiedzwo@gmail.com>
2024-10-19 19:44:50 +0200

    quick rebuild

--- configuration.nix
+++ configuration.nix
@@ -159,6 +159,37 @@
   security.rtkit.enable = true;
   security.polkit.enable = true;
   services.pipewire = {
+    # low latency alsa
+    # wireplumber.extraLuaConfig.main."99-alsa-lowlatency" = ''
+    #   alsa_monitor.rules = {
+    #     {
+    #       matches = {{{ "node.name", "matches", "alsa_output.*" }}};
+    #       apply_properties = {
+    #         ["audio.format"] = "S32LE",
+    #         ["audio.rate"] = "96000", -- for USB soundcards it should be twice your desired rate
+    #         ["api.alsa.period-size"] = 2, -- defaults to 1024, tweak by trial-and-error
+    #         -- ["api.alsa.disable-batch"] = true, -- generally, USB soundcards use the batch mode
+    #       },
+    #     },
+    #   }
+    # '';
+
+    wireplumber.configPackages = [
+      (pkgs.writeTextDir "share/wireplumber/main.lua.d/99-alsa-lowlatency.lua" ''
+        alsa_monitor.rules = {
+          {
+            matches = {{{ "node.name", "matches", "alsa_output.*" }}};
+            apply_properties = {
+              ["audio.format"] = "S32LE",
+              ["audio.rate"] = "96000", -- for USB soundcards it should be twice your desired rate
+              ["api.alsa.period-size"] = 2, -- defaults to 1024, tweak by trial-and-error
+              ["api.alsa.disable-batch"] = true, -- generally, USB soundcards use the batch mode
+            },
+          },
+        }
+      '')
+    ];
+
     extraConfig.pipewire = {
       "91-null-sinks" = {
         "context.objects" = [
