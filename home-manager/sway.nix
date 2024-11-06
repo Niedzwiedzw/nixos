591e51f5de690a008a037c0f535ac3b46b637a80
niedzwiedzw <niedzwiedzwo@gmail.com>
2024-11-06 21:20:41 +0100

    quick rebuild

--- home-manager/sway.nix
+++ home-manager/sway.nix
@@ -97,7 +97,7 @@
         {command = "xrandr --output HDMI-A-1 --primary";}
         {command = "thunderbird";}
         {command = "autotiling";}
-        {command = "kdeconnect";}
+        {command = "kdeconnect-app";}
       ];
       modifier = "Mod4";
       menu = "${pkgs.dmenu}/bin/dmenu_path | ${pkgs.dmenu}/bin/dmenu -b | ${pkgs.findutils}/bin/xargs swaymsg exec --";
