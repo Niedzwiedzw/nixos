f1ba5eb7ef82ecc1a2535aaaf2a2101864a0b22d
niedzwiedzw <niedzwiedzwo@gmail.com>
2024-12-04 20:12:00 +0100

    fixing bug in mesa or gpu drivers - use mesa git

--- configuration.nix
+++ configuration.nix
@@ -94,21 +94,8 @@
   # amd gpu specific stuff
   boot.initrd.kernelModules = ["amdgpu"];
   services.xserver.videoDrivers = ["amdgpu"];
-  hardware = {
-    #
-    #
-    graphics = {
-      enable = true;
-      enable32Bit = true;
-      # driSupport = true;
-      # driSupport32Bit = true;
-      extraPackages = with pkgs; [
-        rocmPackages.clr
-      ];
-      extraPackages32 = with pkgs; [
-      ];
-    };
-  };
+  chaotic.mesa-git.enable = true;
+  hardware.graphics.enable = true;
   # environment.variables.AMD_VULKAN_ICD = "RADV";
   # end of amd gpu specific stuff
 
