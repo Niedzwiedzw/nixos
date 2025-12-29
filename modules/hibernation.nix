a8bc01e3525fbc92fe8f4fee3ad62c8f18457d64
niedzwiedzw <niedzwiedzwo@gmail.com>
2025-12-29 09:58:45 +0100

    migration to 25.11

--- modules/hibernation.nix
+++ modules/hibernation.nix
@@ -1,5 +1,4 @@
 {pkgs, ...}: let
-  rootUUID = "5c5c04d6-36cb-48e5-aa18-201254f0d95d";
   swapUUID = "58e44a76-a337-43cc-97ff-adf1528670b2";
 in {
   powerManagement.enable = true;
@@ -7,10 +6,13 @@ in {
     {
       device = "/dev/disk/by-uuid/${swapUUID}";
     }
-  ]; # 16GB swap file
-  boot.kernelParams = ["mem_sleep_default=deep"]; # Replace 123456 with your swap offset
-  boot.resumeDevice = "/dev/disk/by-uuid/${rootUUID}"; # Replace with your root UUID
-  services.logind.powerKey = "hibernate"; # Power button triggers hibernation
-  services.logind.powerKeyLongPress = "poweroff"; # Long press powers off
+  ];
+  boot.kernelParams = [
+    "mem_sleep_default=deep"
+    "resume=/dev/disk/by-uuid/${swapUUID}"
+  ];
+  boot.resumeDevice = "/dev/disk/by-uuid/${swapUUID}";
+  services.logind.powerKey = "hibernate";
+  services.logind.powerKeyLongPress = "poweroff";
   environment.systemPackages = with pkgs; [swayidle swaylock];
 }
