657f98b7fbda8103600cd61ffd79b4eb8298f91a
niedzwiedzw <niedzwiedzwo@gmail.com>
2026-05-23 20:23:14 +0200

    wip

--- home-manager/display-setups/pc-gpu.nix
+++ home-manager/display-setups/pc-gpu.nix
@@ -1,28 +1,28 @@
-{pkgs, ...}: let
+{...}: let
   displays = import ./available-displays--gpu-pc.nix;
   inherit (displays) iiyama lg aoc sony_tv;
   assignWorkspaces = import ./assign-workspaces.nix;
 in {
-  home.packages = with pkgs; [
-    wl-mirror
-  ];
+  # home.packages = with pkgs; [
+  #   wl-mirror
+  # ];
   wayland.windowManager.sway = {
-    extraConfig = ''
-      # Put wl-mirror on workspace 99 and make it fullscreen
-      workspace 99 output ${sony_tv.name}
-      for_window [app_id="wl-mirror"] {
-          move to workspace 99
-          fullscreen enable
-          floating enable
-          sticky enable          # optional
-      }
+    # extraConfig = ''
+    #   # Put wl-mirror on workspace 99 and make it fullscreen
+    #   workspace 99 output ${sony_tv.name}
+    #   for_window [app_id="wl-mirror"] {
+    #       move to workspace 99
+    #       fullscreen enable
+    #       floating enable
+    #       sticky enable          # optional
+    #   }
 
-      # Prevent normal windows from going to workspace 99
-      for_window [workspace="99"] move to output ${iiyama.name}
-    '';
+    #   # Prevent normal windows from going to workspace 99
+    #   for_window [workspace="99"] move to output ${iiyama.name}
+    # '';
 
     config = {
-      startup = [{command = "wl-mirror --fullscreen-output $mirror_output ${aoc.name}";}];
+      # startup = [{command = "wl-mirror --fullscreen-output $mirror_output ${aoc.name}";}];
       workspaceOutputAssign =
         (assignWorkspaces lg [2 3 4])
         ++ (assignWorkspaces iiyama [1 5 6 7])
